d6a483cbd26f28bfaee7a442ac892f920bb1311e
hossein <ah.rahimi19219@gmail.com>
2022-12-04 11:31:30 +0330

    back connection

--- src/Components/table-managment/Table_managment.jsx
+++ src/Components/table-managment/Table_managment.jsx
@@ -1,35 +1,141 @@
 import React from "react";
-import { useEffect } from "react";
-import { useState } from "react";
+import { useEffect , useState } from "react";
 import { getTableRestaurant } from "../../services/axios";
+import { addTable , deleteTable } from "../../services/axios";
 
 import {
+    notification,
     Modal,
-    AutoComplete,
-    Tag,
-    Tooltip,
-    Button,
-    Cascader,
-    Checkbox,
-    Col,
     Form,
-    Input,
-    message,
     InputNumber,
-    Row,
-    Select,
-    Upload,
   } from 'antd';
 
 import './table-managment.css'
 import 'antd/dist/reset.css';
 
+const TableFormAdd = ({ open, onCreate, onCancel}) => {
+  //console.log(open)
+  const onChange = (value) => {
+    console.log('changed', value);
+  };
+  const [form] = Form.useForm();
+
+return (
+  <Modal className="modal" title="Add table" open={open} onCancel={onCancel}
+    onOk={() => {
+      form.validateFields()
+      .then((values) => {
+        form.resetFields();
+        onCreate(values);
+      })
+      .catch((info) => {
+        console.log(info)
+      })
+    }} 
+    >
+      
+      <Form className='form'
+        name="add_table"
+        layout='horizontal'
+        size='large'
+        form={form}
+        scrollToFirstError
+        >
+        
+        <div className="div-form-items-add">
+        <Form.Item 
+          name="number"
+          label="table number"
+          rules={[
+            {
+              required: true,
+              message: 'Please input table number!'
+            },
+          ]}
+        >
+          <InputNumber className="input" min={1} max={20} defaultValue={0} onChange={onChange} />
+        </Form.Item>
+
+        <Form.Item 
+          name="capacity"
+          label="capacity"
+          rules={[
+            {
+              required: true,
+              message: 'Please input table capacity!'
+            },
+          ]}
+          >
+          <InputNumber className="input" min={1} max={10} defaultValue={0} onChange={onChange} />
+        </Form.Item>
+        </div>
+
+      </Form>
+  </Modal>
+);
+}
+
+const TableFormRemove = ({ open, onCreate, onCancel}) => {
+  //console.log(open)
+  const onChange = (value) => {
+    console.log('changed', value);
+  };
+  const [form] = Form.useForm();
+
+  return (
+    <Modal className="modal" title="remove table" open={open} onCancel={onCancel}
+    onOk={() => {
+      form.validateFields()
+      .then((values) => {
+        form.resetFields();
+        onCreate(values);
+      })
+      .catch((info) => {
+        console.log(info)
+      })
+    }} 
+        >
+        <div>
+        <Form className='form'
+          layout='horizontal'
+          size='large'
+          form={form}
+          name="remove_table"
+          scrollToFirstError
+          >
+
+          <Form.Item className="form-item"
+            name="number"
+            label="table number"
+            rules={[
+              {
+                required: true,
+                message: 'Please input table number!',
+              },
+            ]}
+          >
+            <InputNumber className="input" min={1} max={20} defaultValue={0} onChange={onChange} />
+          </Form.Item>
+        </Form>
+        </div>
+        
+    </Modal>
+  );
+}
+
 export const Table_managment = () => {
+  const id = 1;
+
+  const [isRemoveModalOpen, setIsRemoveModalOpen] = useState(false);
+  const [isAddModalOpen, setIsAddModalOpen] = useState(false);
+  const [tables, setTables] = useState()
 
   useEffect(() => {
-    getTableRestaurant()
+    getTableRestaurant(id)
     .then((response)=>{
-      console.log("response : ", response.data)
+      console.log("all of tables : ", response.data)
+      setTables(response.data)
+      console.log('tables list : ',tables)
     })
   .catch(function (error) {
       if (error.response) {
@@ -41,149 +147,144 @@ export const Table_managment = () => {
         
       } else {
         console.log('Error', error.message);
-      }
+      } 
       console.log(error.config);
     })
   }, [])
 
-    const [isRemoveModalOpen, setIsRemoveModalOpen] = useState(false);
-    const [isAddModalOpen, setIsAddModalOpen] = useState(false);
-
     const showModal_add = () => {
-        setIsAddModalOpen(true);
-    };
+      setIsAddModalOpen(true);
+  };
 
     const showModal_rem = () => {
         setIsRemoveModalOpen(true);
     };
-
-    const handleOk_add = () => {
-        setIsAddModalOpen(false);
-    };
-
-    const handleOk_rem = () => {
-      setIsRemoveModalOpen(false);
-  };
-
 
     const handleCancel = () => {
         setIsAddModalOpen(false);
         setIsRemoveModalOpen(false);
     };
 
-    const onChange = (value) => {
-        console.log('changed', value);
-      };
-
-    const [form] = Form.useForm();
-    
-    const onFinish = (values) => {
+    const onCreateTable = (values) => {
       console.log('Received values of form: ', values);
+
+      const data = ({
+        'restaurantId' : id,
+        'number' : values.number,
+        'capacity' : values.capacity,
+        "reserveTables" : []
+      })
+
+      addTable(data)
+      .then((response) => {
+        console.log(response)
+        notification.open({
+          message: 'successful',
+          //description: error.response.data,
+          type:'success',
+          style: {borderRadius: '5px', backgroundColor: '#fbc403'}
+        });
+        setIsAddModalOpen(false);
+      })
+      .catch(function (error) {
+        notification.open({
+          message: 'Unsuccessful!',
+          description: error.response.data,
+          type:'error',
+          style: {borderRadius: '5px', backgroundColor: '#fbc403'}
+        });
+        if (error.response) {
+          console.log(error.response.data);
+          console.log(error.response.status);
+          console.log(error.response.headers);
+        } else if (error.request) {
+          console.log(error.request);
+          
+        } else {
+          console.log('Error', error.message);
+        }
+        console.log(error.config);
+      })
+
+      
     };
 
 
-  const add_table_gen = () => {
-
-    return (
-      <Modal className="modal" title="Add table" open={isAddModalOpen} onCancel={handleCancel} 
-        footer={
-
-          <Form.Item>
-            <button className='enter-button-add' htmlType="submit" onClick={handleOk_add}>
-              Enter
-            </button>
-          </Form.Item>}>
+    const [id_table, setIdTable]= useState(-1)
+    const [flag, setFlag] = useState(false)
+    const onRemoveTable = (values) => {
+      console.log('Received values of form: ', values);
+      const tmp=[]
+      tables.forEach(item=>{
+        console.log(item);
+        tmp.push({
+          id : item.id,
+          number : item.number
+        })
+      })
+      console.log(values.number)
+      tmp.forEach((num) => { 
+        console.log(num.id)
+        if (num.number === values.number){
+          setIdTable(num.id)
+          setFlag(true)
+        }
+      })
+      console.log(flag)
+      if (!flag){
+        notification.open({
+          message: 'error',
+          description: 'There is no table with this number!', 
+          type:'error',
+          style: {borderRadius: '5px', backgroundColor: '#fbc403'}
+        });
+        return; 
+      }
+      deleteTable(id_table)
+      .then((response) => {
+        console.log(response)
+        
+        setIsRemoveModalOpen(false);
+      })
+      .catch(function (error) {
+        notification.open({
+          message: 'Unsuccessful',
+          description: error.response.data,
+          type:'error',
+          style: {borderRadius: '5px', backgroundColor: '#fbc403'}
+        });
+        if (error.response) {
+          console.log(error.response.data);
+          console.log(error.response.status);
+          console.log(error.response.headers);
+        } else if (error.request) {
+          console.log(error.request);
           
-          <Form className='form'
-            layout='horizontal'
-            size='large'
-            form={form}
-            name="register"
-            onFinish={onFinish}
-            scrollToFirstError
-            >
-            
-            <div className="div-form-items-add">
-            <Form.Item 
-              name="number"
-              label="table number"
-              rules={[
-                {
-                  required: true,
-                  message: 'Please input table number!',
-                },
-              ]}
-            >
-              <InputNumber className="input" min={1} max={20} defaultValue={0} onChange={onChange} />
-            </Form.Item>
+        } else {
+          console.log('Error', error.message);
+        }
+        console.log(error.config);
+      })
 
-            <Form.Item 
-              name="capacity"
-              label="capacity"
-              rules={[
-                {
-                  required: true,
-                  message: 'Please input table capacity!',
-                },
-              ]}
-              >
-              <InputNumber className="input" min={1} max={10} defaultValue={0} onChange={onChange} />
-            </Form.Item>
-            </div>
 
-          </Form>
-      </Modal>
-    );
-  }
-
-  const remove_table_gen = () => {
-
-    return (
-      <Modal className="modal" title="remove table" open={isRemoveModalOpen} onCancel={handleCancel} 
-        footer={
-
-          <Form.Item>
-            <button className='enter-button-remove' htmlType="submit" onClick={handleOk_rem}>
-              Enter
-            </button>
-          </Form.Item>}
-          >
-          <div>
-          <Form className='form'
-            layout='horizontal'
-            size='large'
-            form={form}
-            name="register"
-            onFinish={onFinish}
-            scrollToFirstError
-            >
-
-            <Form.Item className="form-item"
-              name="number"
-              label="table number"
-              rules={[
-                {
-                  required: true,
-                  message: 'Please input table number!',
-                },
-              ]}
-            >
-              <InputNumber className="input" min={1} max={20} defaultValue={0} onChange={onChange} />
-            </Form.Item>
-          </Form>
-          </div>
-          
-      </Modal>
-    );
-  }
+      setIsRemoveModalOpen(false);
+    };
 
     return (
         <div className="table-managment">
             <div className="table-list">
                 <button className="add" onClick={showModal_add}>add</button>
                 <button className="remove" onClick={showModal_rem}>remove</button>
-                {add_table_gen()}
-                {remove_table_gen()}
+                <TableFormAdd
+                  open={isAddModalOpen}
+                  onCreate={onCreateTable}
+                  onCancel={handleCancel}
+                  />
+                <TableFormRemove
+                  open={isRemoveModalOpen}
+                  onCreate={onRemoveTable}
+                  onCancel={handleCancel}
+                  />
             </div>
         </div>
     )
--- src/services/axios.js
+++ src/services/axios.js
@@ -13,6 +13,14 @@ export function getRestaurantCards (){
     return axios.get(url+"api/Restaurant?tag=all&size=10&number=0")
 }
 
-export function getTableRestaurant () {
-    return axios.get(url+'api/Table/id?id=1')
+export function getTableRestaurant (id) {
+    return axios.get(url+'api/Table/by restaurant?restaurantid='+id)
+}
+
+export function addTable (data) {
+    return axios.put(url+'api/Table', data)
+}
+
+export function deleteTable (id) {
+    return axios.delete(url+'api/Table/:'+id)
 }

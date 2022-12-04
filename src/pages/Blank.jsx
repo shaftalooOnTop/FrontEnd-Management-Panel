68e8df0b91bad0f7a41d9aecfc94dce25401dff0
Amir Deldar <amirdldr@gmail.com>
2022-12-04 17:57:15 +0330

    under-construction page added

--- src/pages/Blank.jsx
+++ src/pages/Blank.jsx
@@ -1,6 +1,7 @@
 import './blank.css';
 const Blank = () => {
     return <div className="blank">
+        {/* <img src="https://static.vecteezy.com/system/resources/previews/000/192/083/original/vector-under-construction-landing-page.jpg"></img> */}
     </div>;
 };
 

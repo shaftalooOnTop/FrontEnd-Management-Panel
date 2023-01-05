33fe1b9b0d2dd323d6995b00fc8b89cceb15676b
Amir Deldar <amirdldr@gmail.com>
2023-01-05 10:30:27 +0330

    some styles added

--- src/Components/Sale-report/Sale-report.jsx
+++ src/Components/Sale-report/Sale-report.jsx
@@ -62,10 +62,12 @@ export const Sale_report = () => {
 
     getBusiestTime(restId)
       .then((e) => {
-        setDailyBusiestTime(e.data.dayHour.length > 0 ? e.data.dayHour : 0);
-        setWeeklyBusiestTime(e.data.weekHour.length > 0 ? e.data.weekHour : 0);
+        setDailyBusiestTime(e.data.dayHour.length > 0 ? e.data.dayHour[0] : 0);
+        setWeeklyBusiestTime(
+          e.data.weekHour.length > 0 ? e.data.weekHour[0] : 0
+        );
         setMonthlyBusiestTime(
-          e.data.monthHour.length > 0 ? e.data.monthHour : 0
+          e.data.monthHour.length > 0 ? e.data.monthHour[0] : 0
         );
       })
       .catch();
@@ -81,151 +83,172 @@ export const Sale_report = () => {
 
   return (
     <>
-    <div className="report-all">
-    <div>
-        <div className="sale-report-cards">
-          <div
-            className="card text-white  mb-3"
-            style={{ maxWidth: "18rem", minWidth: "18rem", backgroundColor:"#2A6877"}}
-          >
-            <div className="card-header">Daily Profit</div>
-            <div className="card-body">
-              <h5 className="card-title">{current_day}</h5>
-              <p className="card-text">{dailyProfit} Toman</p>
+      <div className="report-all">
+        <div>
+          <div className="sale-report-cards">
+            <div
+              className="card text-white  mb-3"
+              style={{
+                maxWidth: "33%",
+                minWidth: "33%",
+                backgroundColor: "#2A6877",
+              }}
+            >
+              <div className="card-header">Daily Profit</div>
+              <div className="card-body">
+                <h5 className="card-title">{current_day}</h5>
+                <p className="card-text">{dailyProfit} Toman</p>
+              </div>
             </div>
-          </div>
 
-          <div
-            className="card text-white  mb-3"
-            style={{ maxWidth: "20%", minWidth: "30%", backgroundColor:"#406B58"}}
-          >
-            <div className="card-header">Monthly Profit</div>
-            <div className="card-body">
-              <h5 className="card-title">{current_month}</h5>
-              <p className="card-text">{monthlyProfit} Toman</p>
+            <div
+              className="card text-white  mb-3"
+              style={{
+                maxWidth: "30%",
+                minWidth: "30%",
+                backgroundColor: "#406B58",
+              }}
+            >
+              <div className="card-header">Monthly Profit</div>
+              <div className="card-body">
+                <h5 className="card-title">{current_month}</h5>
+                <p className="card-text">{monthlyProfit} Toman</p>
+              </div>
             </div>
-          </div>
-          <div
-            className="card text-white mb-3"
-            style={{ maxWidth: "30%", minWidth: "30%", backgroundColor:"#B92B27" }}
-          >
-            <div className="card-header">Yearly Profit</div>
-            <div className="card-body">
-              <h5 className="card-title">{current_year}</h5>
-              <p className="card-text">{yearlyProfit} Toman</p>
+            <div
+              className="card text-white mb-3"
+              style={{
+                maxWidth: "30%",
+                minWidth: "30%",
+                backgroundColor: "#B92B27",
+              }}
+            >
+              <div className="card-header">Yearly Profit</div>
+              <div className="card-body">
+                <h5 className="card-title">{current_year}</h5>
+                <p className="card-text">{yearlyProfit} Toman</p>
+              </div>
             </div>
-          </div>
-        </div>
-        <div className="restBusi" style={{ display: "flex" }}>
-          <div
-            className="card text-black bg-warning mb-3 restBusi2"
             
-          >
-            <div className="card-header res-busiest-card">
-              Restaurant Busiest Times
-            </div>
-            <div className="card-body clock-body">
-              <div className="clock-bar">
-                <div className="clock-item">
-                  <CircularProgressbar
-                    value={
-                      ((dailyBusiestTime > 12
-                        ? dailyBusiestTime - 12
-                        : dailyBusiestTime) /
-                        12) *
-                      100
-                    }
-                    text={dailyBusiestTime}
-                    styles={buildStyles({
-                      rotation: 0,
-                      strokeLinecap: "butt",
-                      textSize: "16px",
-                      pathTransitionDuration: 0.5,
-                      pathColor: "#161616",
-                      textColor: "#161616",
-                      //   trailColor: 'white',
-                    })}
-                  />
-                  <p className="clock-item-title">Daily</p>
-                </div>
-                <div className="clock-item">
-                  <CircularProgressbar
-                    value={
-                      ((weeklyBusiestTime > 12
-                        ? weeklyBusiestTime - 12
-                        : weeklyBusiestTime) /
-                        12) *
-                      100
-                    }
-                    text={weeklyBusiestTime}
-                    styles={buildStyles({
-                      rotation: 0,
-                      strokeLinecap: "butt",
-                      textSize: "16px",
-                      pathTransitionDuration: 0.5,
-                      pathColor: "#161616",
-                      textColor: "#161616",
-                      //   trailColor: 'white',
-                    })}
-                  />
-                  <p className="clock-item-title">Weekly</p>
-                </div>
-                <div className="clock-item">
-                  <CircularProgressbar
-                    value={
-                      ((monthlyBusiestTime > 12
-                        ? monthlyBusiestTime - 12
-                        : monthlyBusiestTime) /
-                        12) *
-                      100
-                    }
-                    text={monthlyBusiestTime}
-                    styles={buildStyles({
-                      rotation: 0,
-                      strokeLinecap: "butt",
-                      textSize: "16px",
-                      pathTransitionDuration: 0.5,
-                      pathColor: "#161616",
-                      textColor: "#161616",
-                      //   trailColor: 'white',
-                    })}
-                  />
-                  <p className="clock-item-title">Monthly</p>
+          </div>
+          <div className="restBusi" style={{ display: "flex" }}>
+            <div className="card text-black bg-warning mb-3 restBusi2">
+              <div className="card-header res-busiest-card">
+                Restaurant Busiest Times
+              </div>
+              <div className="card-body clock-body">
+                <div className="clock-bar">
+                  <div className="clock-item">
+                    <CircularProgressbar
+                      value={
+                        ((dailyBusiestTime > 12
+                          ? dailyBusiestTime - 12
+                          : dailyBusiestTime) /
+                          12) *
+                        100
+                      }
+                      text={dailyBusiestTime}
+                      styles={buildStyles({
+                        rotation: 0,
+                        strokeLinecap: "butt",
+                        textSize: "16px",
+                        pathTransitionDuration: 0.5,
+                        pathColor: "#161616",
+                        textColor: "#161616",
+                        //   trailColor: 'white',
+                      })}
+                    />
+                    <p className="clock-item-title">Daily</p>
+                  </div>
+                  <div className="clock-item">
+                    <CircularProgressbar
+                      value={
+                        ((weeklyBusiestTime > 12
+                          ? weeklyBusiestTime - 12
+                          : weeklyBusiestTime) /
+                          12) *
+                        100
+                      }
+                      text={weeklyBusiestTime}
+                      styles={buildStyles({
+                        rotation: 0,
+                        strokeLinecap: "butt",
+                        textSize: "16px",
+                        pathTransitionDuration: 0.5,
+                        pathColor: "#161616",
+                        textColor: "#161616",
+                        //   trailColor: 'white',
+                      })}
+                    />
+                    <p className="clock-item-title">Weekly</p>
+                  </div>
+                  <div className="clock-item">
+                    <CircularProgressbar
+                      value={
+                        ((monthlyBusiestTime > 12
+                          ? monthlyBusiestTime - 12
+                          : monthlyBusiestTime) /
+                          12) *
+                        100
+                      }
+                      text={monthlyBusiestTime}
+                      styles={buildStyles({
+                        rotation: 0,
+                        strokeLinecap: "butt",
+                        textSize: "16px",
+                        pathTransitionDuration: 0.5,
+                        pathColor: "#161616",
+                        textColor: "#161616",
+                        //   trailColor: 'white',
+                      })}
+                    />
+                    <p className="clock-item-title">Monthly</p>
+                  </div>
                 </div>
               </div>
             </div>
-          </div>
-          <div className="card text-white mb-3" style={{ maxWidth: "30%", minWidth: "30%", backgroundColor:"#F76704" }}>
-            <div className="card-header">Best Selling Foods</div>
-            <div className="card-body">
-              {foods?.slice(0, 3).map((x) => (
-                <div className="best-selling-all">
-                  <img
-                    className="best-selling-img"
-                    src={url + "api/www/ImgGet/" + x.key.photo.id}
-                    alt=""
-                  ></img>
-                  <div className="best-selling-details">
-                    <h6 className="card-text">{x.key.name}</h6>
-                    <p>Sales amount: {x.value}</p>
+            <div
+              className="card text-white mb-3"
+              style={{
+                maxWidth: "30%",
+                minWidth: "30%",
+                backgroundColor: "#F76704",
+              }}
+            >
+              <div className="card-header">Best Selling Foods</div>
+              <div className="card-body">
+                {foods?.slice(0, 3).map((x) => (
+                  <div className="best-selling-all">
+                    <img
+                      className="best-selling-img"
+                      src={url + "api/www/ImgGet/" + x.key.photo.id}
+                      alt=""
+                    ></img>
+                    <div className="best-selling-details">
+                      <h6 className="card-text">{x.key.name}</h6>
+                      <p>Sales amount: {x.value}</p>
+                    </div>
                   </div>
-                </div>
-              ))}
+                ))}
+              </div>
             </div>
           </div>
         </div>
+        <div
+              className="card text-white  mb-5"
+              style={{
+                maxWidth: "20%",
+                minWidth: "20%",
+                backgroundColor: "#37505E",
+              }}
+            >
+              <div className="card-header">Rank Tables</div>
+              <div className="card-body">
+                <h5 className="card-title">First Tables</h5>
+                <p className="card-text">Test</p>
+              </div>
+        </div>
       </div>
-      <div
-            className="card text-white  mb-5"
-            style={{ maxWidth: "20%", minWidth: "20%", backgroundColor:"#37505E"}}
-          >
-            <div className="card-header">Rank Tables</div>
-            <div className="card-body">
-              <h5 className="card-title">First Tables</h5>
-              <p className="card-text">Test</p>
-            </div>
-    </div>
-    </div>
     </>
   );
 };
